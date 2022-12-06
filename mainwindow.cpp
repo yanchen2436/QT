6e218cde8c684d2fdb2dac22ee642f4a96332db7
lhy <lhyqaq@qq.com>
2022-12-06 11:44:20 +0800

    新增socket功能模块

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    tcpsocket = new QTcpSocket(this);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +14,35 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+
+
+void MainWindow::connected_SLOT()
+{
+    connect(tcpsocket, SIGNAL(readyRead()), this, SLOT(readyRead_Slot())); //将信号连接到槽，书写比较明确
+}
+void MainWindow::readyRead_Slot()//定义接收信号的槽
+{
+    QString buf;
+    buf = tcpsocket->readAll();
+    ui->receivewd->appendPlainText(buf);//接收由tcp发送过来的信息
+}
+
+
+void MainWindow::on_open_client_clicked()
+{
+    tcpsocket->connectToHost(ui->ipnum->text(), ui->portnum->text().toUInt()); //转为无符号，连接服务器端口
+    connect(tcpsocket, SIGNAL(connected()), this, SLOT(connected_SLOT()));
+    printf("打开客户端 ");
+}
+
+void MainWindow::on_close_client_clicked()
+{
+    tcpsocket->close();
+    printf("关闭客户端 ");
+}
+
+void MainWindow::on_send_clicked()
+{
+    tcpsocket->write(ui->sendText->text().toLocal8Bit().data());
+}
